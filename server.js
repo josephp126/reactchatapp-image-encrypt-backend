aacfdd1d7bbb585651273fb9ddecae915941890e
rockstar0711 <happpybopy123@outlook.com>
2021-11-10 16:37:58 +0800

    cors token & port

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const dbConfig = require('./app/config/db.config')
 const app = express();
 
 var corsOptions = {
-  origin: "http://localhost:8081",
+  // origin: "http://localhost:8081",
+  origin: "http://192.168.101.109:3000",
 };
 
 app.use(cors(corsOptions));
@@ -68,7 +69,7 @@ require('./app/routes/auth.routes')(app);
 require('./app/routes/user.routes')(app);
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
