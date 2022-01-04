6f6f0490b4526822adcfe12dad9e4078bec7f51d
rockstar0711 <happpybopy123@outlook.com>
2022-01-04 17:38:06 +0200

    friend request

--- app/controllers/friends.controller.js
+++ app/controllers/friends.controller.js
@@ -4,17 +4,52 @@ const Users = db.user;
 
 //new friends
 exports.newFriend = async (req, res) => {
-    const newFriend = new Friends({
-        friends: [req.body.senderId, req.body.receiverId],
-        friend:req.body.receiverId
-    });
-    try {
-        const savedNewFriend = await newFriend.save();
-        res.status(200).json(newFriend);
+    const requestedFriend = await Users.findOne({ email: req.body.friendEmail });
+    console.log(requestedFriend);
+    if (!requestedFriend) {
+        res.status(500).json({ msg: 'notExist' });
     }
-    catch (err) {
-        res.status(500).json(err);
+    else {
+        let isAlreadyFriend = false;
+        let friends = await Friends.find({
+            friends: { $in: [req.body.senderId] },
+        });
+        let friendsData = JSON.parse(JSON.stringify(friends))
+        await Promise.all(friendsData.map(async (friend) => {
+            const friendId = friend.friends.find((m) => m !== req.body.senderId);
+            if (friendId == requestedFriend._id) {
+                isAlreadyFriend = true
+            }
+        }))
+
+        //if this friend's email is mine?
+        if (requestedFriend._id == req.body.senderId) {
+            res.status(500).json({ msg: 'impossible' });
+        }
+        //if this email's owner is already friend?
+        else if (isAlreadyFriend) {
+            res.status(500).json({ msg: 'alreadyFriend' });
+        }
+        //if requested email does not exist
+
+
+        //save new Friend
+        else {
+            const newFriend = new Friends({
+                friends: [req.body.senderId, requestedFriend._id.toString()],
+                friend: requestedFriend._id
+            });
+            try {
+                const savedNewFriend = await newFriend.save();
+                await oppositeNewFriend.save();
+                res.status(200).json(newFriend);
+            }
+            catch (err) {
+                res.status(500).json(err);
+            }
+        }
     }
+
 }
 
 //get friends of a user
@@ -24,20 +59,21 @@ exports.getFriends = async (req, res) => {
         //     friends: { $in: [req.params.userId] },
         // }).populate("friend", "-id -_id -__v -email -password -roles -createdAt -updatedAt").select("-__v");
         // res.status(200).json(friends);
+        console.log(req.params.userId)
         let friends = await Friends.find({
             friends: { $in: [req.params.userId] },
         });
         let friendsData = JSON.parse(JSON.stringify(friends))
-        // console.log('friends:', friends)
+        console.log('friends:', friends)
         let userList = []
-        await Promise.all(friendsData.map( async (friend,index) => {
+        await Promise.all(friendsData.map(async (friend, index) => {
             const friendId = friend.friends.find((m) => m !== req.params.userId);
-            const user = await Users.findOne({_id : friendId})
+            const user = await Users.findOne({ _id: friendId })
             friendsData[index].friend = JSON.parse(JSON.stringify(user))
-            console.log('----------',friendsData[index])
+            console.log('----------', friendsData[index])
             // userList = userList.push(user)
         }))
-        console.log("==========",friendsData)
+        console.log("==========", friendsData)
         // let friendList = friends.populate("friend", "-id -_id -__v -email -password -roles -createdAt -updatedAt").select("-__v");
         res.status(200).json(friendsData);
     }
